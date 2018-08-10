72bc76575e9c396fa43d432885d2181fd48de8fd
liao <1256636645@qq.com>
2018-08-10 17:01:19 +0800

    commit 1

---
+++ README.md
@@ -0,0 +1,109 @@
+# zabbix-smartmontools
+## Features
+Cross-platform SMART monitoring scripts with two display modes: [device](https://github.com/nobodysu/zabbix-smartmontools/blob/master/screenshots/smartctl_mode-device-example.png?raw=true) and [serial](https://github.com/nobodysu/zabbix-smartmontools/blob/master/screenshots/smartctl_mode-serial-example.png?raw=true). LLD discovers and sends data in one pass, using minimal number of utilities. Supports any SMART name and displays it as is.
+
+#### Advantages
+- Utilises smartctl error return codes
+- Full Low-Level Discovery: there is no need to add any SMART items
+- Efficient: no unnecessary processes are spawned
+- Bulk items upload with zabbix-sender
+- Error-proof configuration: you'll know when host is not configured
+
+#### Disadvantages
+- Requires configuration
+- Manual RAID passthrough
+- Semi-hardcoded triggers (still for all disks though)
+
+![Triggers-Discovery](https://raw.githubusercontent.com/nobodysu/zabbix-smartmontools/master/screenshots/smartctl_discovery_triggers_cut.png)
+
+![Triggers](https://raw.githubusercontent.com/nobodysu/zabbix-smartmontools/master/screenshots/smartctl_triggers_cut.png)
+
+## Installation
+As prerequisites you need `python3`, `smartmontools`, `sudo` and `zabbix-sender` packages. For testing, `zabbix-get` is also required.
+<br />
+Take a look at scripts first lines and provide paths if needed. If you have a RAID configuration, also provide that by hand. Choose `device` or `serial` mode. Import `Template_App_smartmontools.xml` in zabbix web interface.
+
+### First step
+#### Linux
+```bash
+mv smartctl-lld.py smartctl-send.py /etc/zabbix/scripts/
+mv sudoers.d/zabbix /etc/sudoers.d/   # place sudoers include here for smartctl-lld.py sudo access
+mv userparameter_smartctl.conf /etc/zabbix/zabbix_agentd.d/   # move zabbix items include here
+```
+
+#### FreeBSD
+```bash
+mv smartctl-lld.py smartctl-send.py /usr/local/etc/zabbix/scripts/
+mv sudoers.d/zabbix /usr/local/etc/sudoers.d/
+mv userparameter_smartctl.conf /usr/local/etc/zabbix/zabbix_agentd.d/
+```
+
+#### Windows
+```cmd
+move smartctl-lld.py C:\zabbix-agent\scripts\
+move smartctl-send.py C:\zabbix-agent\scripts\
+move userparameter_smartctl.conf C:\zabbix-agent\zabbix_agentd.conf.d\
+```
+Install `python3` for all users, adding it to `PATH` during installation. Install `smartmontools` and add its bin folder to `PATH` in environment variables.
+<br />
+Note: currently windows version does not detaches and data can only be gathered on second run.
+
+### Second step
+Then you need to include your zabbix conf folder in `zabbix_agentd.conf`, like this:
+```conf
+Include=/usr/local/etc/zabbix/zabbix_agentd.conf.d/
+```
+Also its recomended to add at least `Timeout=5` to config file to allow drives spun up in rare cases.
+
+Thats all for Windows. For others run the following to finish configuration:
+```bash
+chmod 750 scripts/smartctl-*.py   # apply necessary permissions
+chown root:zabbix scripts/smartctl-*.py
+chmod 640 userparameter_smartctl.conf
+chown root:zabbix userparameter_smartctl.conf
+chmod 400 sudoers.d/zabbix
+chown root sudoers.d/zabbix
+visudo   # test sudoers configuration
+```
+
+## Testing
+```bash
+zabbix_get -s 192.0.2.1 -k smartctl.discovery[get,"Example host"]
+```
+Default operation mode. Displays json that server should get, detaches, then waits and sends data with zabbix-sender. `Example host` is your `Host name` field in zabbix.
+<br /><br />
+
+```bash
+zabbix_get -s 192.0.2.1 -k smartctl.discovery[getverb,"Example host"]
+```
+Verbose mode. Does not detaches or prints LLD. Lists all items sent to zabbix-sender, also it is possible to see sender output in this mode.
+<br /><br />
+
+Note: before scripts would work, zabbix server must first discover available items. It is done in 6 hour cycles by default. You can temporary decrease this parameter for testing in `template -> Discovery -> SMART disk discovery -> Update interval`. In this case update value must not be less than 60 seconds.
+
+These scripts were tested to work with following configurations:
+- Centos 7 / Zabbix 2.4 / Python 3.4
+- Debian 8 / ZS (2.4, 3.4) / ZA (2.4, 3.0, 3.2, 3.4) / Python 3.4
+- FreeBSD 10.1 / Zabbix 2.4 / Python 3.4
+- Windows XP / Zabbix 2.4 / Python 3.4
+- Windows 7 / ZS (2.4, 3.4) / ZA (2.4, 3.0, 3.2, 3.4) / Python 3.4
+- Windows Server 2012 / Zabbix 2.4 / Python 3.4
+
+## Updating
+### from any to 1.3, 1.4, 1.5, 1.5.1:<br />
+Replace all old files with new ones and reupload the template.
+
+## Issues
+- Zabbix web panel displays an error on json discovery, but apparently everything works fine
+- Windows version does not detaches, and data will only be gathered on second pass (probably permanent workaround)
+
+## Planned features
+- SSD life monitoring, TRIM capability
+- ERC / TLER / CCTL is-enabled check
+- Disk SMART capability auto-enabling
+
+## Links
+- https://www.smartmontools.org
+- https://unlicense.org
+- [The 5 SMART stats that actually predict hard drive failure](http://www.computerworld.com/article/2846009/the-5-smart-stats-that-actually-predict-hard-drive-failure.html)
+- [Disk and CPU temperature monitoring solution](https://github.com/nobodysu/zabbix-mini-IPMI)
---
+++ scripts/smartctl-lld.py
@@ -0,0 +1,217 @@
+#!/usr/bin/env python3
+
+## Installation instructions: https://github.com/nobodysu/zabbix-smartmontools
+
+mode = 'device'   # 'device' or 'serial' as primary identifier in zabbix item's name
+                  # 'serial' is much safer for multi-disk system
+
+ctlPath = r'smartctl'
+#ctlPath = r'C:\Program Files\smartmontools\bin\smartctl.exe'   # if smartctl isn't in PATH
+#ctlPath = r'/usr/local/sbin/smartctl'
+
+# path to second send script
+senderPyPath = r'/etc/zabbix/scripts/smartctl-send.py'              # Linux
+#senderPyPath = r'C:\zabbix-agent\scripts\smartctl-send.py'         # Win
+#senderPyPath = r'/usr/local/etc/zabbix/scripts/smartctl-send.py'   # BSD
+
+# path to zabbix agent configuration file
+agentConf = r'/etc/zabbix/zabbix_agentd.conf'                       # Linux
+#agentConf = r'C:\zabbix_agentd.conf'                               # Win
+#agentConf = r'/usr/local/etc/zabbix24/zabbix_agentd.conf'          # BSD
+
+senderPath = r'zabbix_sender'                                       # Linux, BSD
+#senderPath = r'C:\zabbix-agent\bin\win32\zabbix_sender.exe'        # Win
+
+timeout = '60'   # how long the script must wait between LLD and sending, increase if data received late (does not affect windows)
+                 # this setting MUST be lower than 'Update interval' in discovery rule
+
+# manually provide disk list or RAID configuration if needed
+diskListManual = []
+# like this:
+#diskListManual = ['/dev/sda -d sat+megaraid,4', '/dev/sda -d sat+megaraid,5']
+# more info: https://www.smartmontools.org/wiki/Supported_RAID-Controllers
+
+## End of configuration ##
+
+import sys
+import subprocess
+import re
+from json import dumps
+from shlex import split
+
+# intended for 'python3' binary but can be changed in special cases, alongside with shebang
+if sys.platform != 'win32':   # if not windows
+    pythonCmd = 'python3'
+else:
+    pythonCmd = 'python.exe'
+
+hostname = '"' + sys.argv[2] + '"'
+
+jsonData = []
+senderData = []
+
+stopChars = [('/dev/', ''), (' -d', ''), (',', ''), ('[', ' '), (']', ' '), ('+', '_'), ('  ', ' '), ('/', '_'), ('~', '_'), ('`', '_'), ('@', '_'), ('#', '_'), ('$', '_'), ('%', '_'), ('^', '_'), ('&', '_'), ('*', '_'), ('(', '_'), (')', '_'), ('{', '_'), ('}', '_'), ('=', '_'), (':', '_'), (';', '_'), ('"', '_'), ('?', '_'), ('<', '_'), ('>', '_'), (' ', '_')]
+
+def replace_all(string, stopChars):
+    for i, j in stopChars:
+        string = string.replace(i, j)
+    return string
+
+allDisksStdout = ''
+configStatusError = ''
+if not diskListManual:   # if manual list is not provided
+    try:
+        allDisksStdout = subprocess.check_output([ctlPath, '--scan'], universal_newlines=True)   # scan the disks
+    except OSError as e:
+        if e.args[0] == 2:
+            configStatusError = 'SCAN_OS_NOCMD'
+        else:
+            configStatusError = 'SCAN_OS_ERROR'
+    except:
+        configStatusError = 'SCAN_UNKNOWN_ERROR'
+
+        if sys.argv[1] == 'getverb':
+            raise
+
+    diskListRe = re.findall(r'^(/dev/[^ ]+)', allDisksStdout, re.M)   # and determine short device names
+else:
+    diskListRe = diskListManual   # or just use manually provided settings
+#print(diskListRe)
+
+
+for d in diskListRe:   # loop through all found drives
+    deviceNameOrig = d
+    d = replace_all(d, stopChars)   # sanitize the item key
+    deviceName = d   # save device name before mode selection and after manual substitution
+    #print('disk:                         ', d)
+
+    try:
+        diskProc = subprocess.check_output([ctlPath, '-a'] + split(deviceNameOrig), universal_newlines=True)   # take string from 'diskListRe', make arguments from it and append to existing command, then run it
+    except OSError as e:
+        if e.args[0] == 2:
+            configStatusError = 'D_OS_NOCMD'
+            break
+        else:
+            configStatusError = 'D_OS_ERROR'
+            break
+    except subprocess.CalledProcessError as e:   # handle process-specific errors
+        ''' see 'man smartctl' for more information
+        Bit 0 = Code 1
+        Bit 1 = Code 2
+        Bit 2 = Code 4
+        Bit 3 = Code 8
+        Bit 4 = Code 16
+        Bit 5 = Code 32
+        Bit 6 = Code 64
+        Bit 7 = Code 128
+        '''
+        diskProc = e.output   # substitude output even on error, so it can be processed further
+        senderData.append(hostname + ' smartctl.info[' + d + ',DriveStatus] "ERR_CODE_' + str(e.args[0]) + '"')
+
+        if e.args[0] == 1 or e.args[0] == 2:
+            continue   # continue to the next disk on fatal error
+    except:
+        diskProc = e.output
+        senderData.append(hostname + ' smartctl.info[' + d + ',DriveStatus] "UNKNOWN_ERROR_ON_PROCESSING"')
+
+        if sys.argv[1] == 'getverb':
+            raise
+    else:
+        senderData.append(hostname + ' smartctl.info[' + d + ',DriveStatus] "PROCESSED"')   # no trigger assigned, but its needed as a fallback value
+    finally:
+        jsonData.append({'{#DDRIVESTATUS}':d})   # it will fire even on OSError (no senderData)
+
+    serialRe = re.search(r'^Serial Number:\s+(.+)$', diskProc, re.M | re.I)
+    #print(d + ': serialRe.group(1):      ' + serialRe.group(1))
+    if serialRe:
+        if mode == 'serial':
+            d = replace_all(serialRe.group(1), stopChars)   # in 'serial' mode, if serial number is found it will be used as main identifier, also sanitize it
+            # ! 'd' becomes serial !
+
+        jsonData.append({'{#DSERIAL}':d})
+        senderData.append(hostname + ' smartctl.info[' + d + ',serial] "' + serialRe.group(1) + '"')
+
+    jsonData.append({'{#DNAME}':d})
+    senderData.append(hostname + ' smartctl.info[' + d + ',device] "' + deviceName + '"')
+
+    modelRe = re.search(r'^Device Model:\s+(.+)$', diskProc, re.M | re.I)
+    #print(d + ': modelRe.group(1):       ' + modelRe.group(1))
+    if modelRe:
+        jsonData.append({'{#DMODEL}':d})
+        senderData.append(hostname + ' smartctl.info[' + d + ',model] "' + modelRe.group(1) + '"')
+
+    firmwareRe = re.search(r'^Firmware Version:\s+(.+)$', diskProc, re.M | re.I)
+    #print(d + ': firmwareRe.group(1):    ' + firmwareRe.group(1))
+    if firmwareRe:
+        jsonData.append({'{#DFIRMWARE}':d})
+        senderData.append(hostname + ' smartctl.info[' + d + ',firmware] "' + firmwareRe.group(1) + '"')
+
+    capacityRe = re.search(r'User Capacity:\s+(.+)bytes', diskProc, re.I)
+    if capacityRe:
+        capacityValue = re.sub('\D', '', capacityRe.group(1))   # substitute all but numbers
+        #print(d + ': capacityValue:          ' + capacityValue)
+        jsonData.append({'{#DCAPACITY}':d})
+        senderData.append(hostname + ' smartctl.info[' + d + ',capacity] "' + capacityValue + '"')
+
+    rpmRe = re.search(r'^Rotation Rate:\s+(\d+)\s+rpm$', diskProc, re.M | re.I)
+    #print(d + ': rpmRe.group(1):         ' + rpmRe.group(1))
+    if rpmRe:
+        jsonData.append({'{#DRPM}':d})
+        senderData.append(hostname + ' smartctl.info[' + d + ',rpm] "' + rpmRe.group(1) + '"')
+
+    selftestRe = re.search(r'^SMART overall-health self-assessment test result:\s+(.+)$', diskProc, re.M | re.I)
+    #print(d + ': selftestRe.group(1):    ' + selftestRe.group(1))
+    if selftestRe:
+        jsonData.append({'{#DSELFTEST}':d})
+        senderData.append(hostname + ' smartctl.info[' + d + ',selftest] "' + selftestRe.group(1) + '"')
+
+    valuesRe = re.findall(r'^(?:\s+)?(\d+)\s+([\w-]+)\s+[\w-]+\s+\d{3}\s+\d{3}\s+\d{3}\s+[\w-]+\s+[\w-]+\s+[\w-]+\s+(\d+)', diskProc, re.M | re.I)   # catch id, name and value
+    #print(d + ': valuesRe:\n', valuesRe)
+
+    jsonData.append({'{#DSMARTSTATUS}':d})
+    if valuesRe:
+        senderData.append(hostname + ' smartctl.info[' + d + ',SmartStatus] "PRESENT"')   # item does not correctly represents action, will be changed in future
+
+        for v in valuesRe:
+            if v[0] == '5':                         # semi-hardcoded values for triggers
+                jsonData.append({'{#DVALUE5}':d})
+            elif v[0] == '187':
+                jsonData.append({'{#DVALUE187}':d})
+            elif v[0] == '188':
+                jsonData.append({'{#DVALUE188}':d})
+            elif v[0] == '197':
+                jsonData.append({'{#DVALUE197}':d})
+            elif v[0] == '198':
+                jsonData.append({'{#DVALUE198}':d})
+            elif v[0] == '199':
+                jsonData.append({'{#DVALUE199}':d})
+            else:
+                jsonData.append({'{#DVALUE}':d, '{#SMARTID}':v[0], '{#SMARTNAME}':v[1]})   # all other possible values
+
+            senderData.append(hostname + ' smartctl.value[' + d + ',' + v[0] + '] ' + v[2])
+
+    else:
+        senderData.append(hostname + ' smartctl.info[' + d + ',SmartStatus] "NO_SMART_VALUES"')
+
+if configStatusError != '':
+    senderData.append(hostname + ' smartctl.info[ConfigStatus] "' + configStatusError + '"')
+elif not diskListRe:
+    senderData.append(hostname + ' smartctl.info[ConfigStatus] "NODISKS"')   # if no disks were found
+else:
+    senderData.append(hostname + ' smartctl.info[ConfigStatus] "CONFIGURED"')   # signals that client host is configured
+
+senderDataNStr = '\n'.join(senderData)   # items for zabbix sender separated by newlines
+
+# pass senderDataNStr to smartctl-send.py:
+if sys.argv[1] == 'get':
+    print(dumps({"data": jsonData}, indent=4))   # print data gathered for LLD
+
+    subprocess.Popen([pythonCmd, senderPyPath, 'get', agentConf, senderPath, timeout, senderDataNStr], stdin=subprocess.PIPE, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)   # spawn new process and regain shell control immediately (on Win 'smartctl-send.py' will not wait)
+
+elif sys.argv[1] == 'getverb':
+    subprocess.Popen([pythonCmd, senderPyPath, 'getverb', agentConf, senderPath, timeout, senderDataNStr], stdin=subprocess.PIPE)   # do not detach if in verbose mode, also skips timeout in 'smartctl-send.py'
+
+else:
+    print(sys.argv[0] + " : Not supported. Use 'get' or 'getverb'.")
+    #sys.exit(1)  # it's safer to just disable error codes after 3.4 changes
+
